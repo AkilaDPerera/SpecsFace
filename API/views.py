cab074ff3e8add69fb62772fe44f954221c01594
Akila D Perera <akilamatrix@gmail.com>
2018-02-26 20:40:24 +0530

    done

--- API/views.py
+++ API/views.py
@@ -15,22 +15,23 @@ def dataUploadingForm(request):
 
 def processImages(request):
     try:
-        face = request.FILES["face"]
-        frame = request.FILES["frame"]
-        fs = FileSystemStorage()
-        face_url = fs.save(face.name, face)
-        frame_url = fs.save(frame.name, frame)
+        # face = request.FILES["face"]
+        # frame = request.FILES["frame"]
+        # fs = FileSystemStorage()
+        # face_url = fs.save(face.name, face)
+        # frame_url = fs.save(frame.name, frame)
 
-        uploaded_face_url = fs.url(face_url)
-        uploaded_frame_url = fs.url(frame_url)
+        # uploaded_face_url = fs.url(face_url)
+        # uploaded_frame_url = fs.url(frame_url)
         
-        face_spec.process_image("."+uploaded_face_url, "."+uploaded_frame_url)
+        # face_spec.process_image("."+uploaded_face_url, "."+uploaded_frame_url)
         
-        # Clear storage
-        fs.delete(face.name)
-        fs.delete(frame.name)
+        # # Clear storage
+        # fs.delete(face.name)
+        # fs.delete(frame.name)
     except:
         return render(request, "404.html")
     else:
-        return HttpResponse(base64.b64encode(open("media/output.jpg", "rb").read()), content_type="image/png")
+        return render(request, "404.html")
+        # return HttpResponse(base64.b64encode(open("media/output.jpg", "rb").read()), content_type="image/png")
 
