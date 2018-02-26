70704a4a8842a47f13963434d88f5e15888a34f8
Akila D Perera <akilamatrix@gmail.com>
2018-02-26 20:43:43 +0530

    done

--- API/views.py
+++ API/views.py
@@ -14,24 +14,21 @@ def dataUploadingForm(request):
     return render(request, "dataUploadingPage.html")
 
 def processImages(request):
-    try:
-        # face = request.FILES["face"]
-        # frame = request.FILES["frame"]
-        # fs = FileSystemStorage()
-        # face_url = fs.save(face.name, face)
-        # frame_url = fs.save(frame.name, frame)
+    return render(request, "404.html")
+    # face = request.FILES["face"]
+    # frame = request.FILES["frame"]
+    # fs = FileSystemStorage()
+    # face_url = fs.save(face.name, face)
+    # frame_url = fs.save(frame.name, frame)
 
-        # uploaded_face_url = fs.url(face_url)
-        # uploaded_frame_url = fs.url(frame_url)
-        
-        # face_spec.process_image("."+uploaded_face_url, "."+uploaded_frame_url)
-        
-        # # Clear storage
-        # fs.delete(face.name)
-        # fs.delete(frame.name)
-    except:
-        return render(request, "404.html")
-    else:
-        return render(request, "404.html")
-        # return HttpResponse(base64.b64encode(open("media/output.jpg", "rb").read()), content_type="image/png")
+    # uploaded_face_url = fs.url(face_url)
+    # uploaded_frame_url = fs.url(frame_url)
+    
+    # face_spec.process_image("."+uploaded_face_url, "."+uploaded_frame_url)
+    
+    # # Clear storage
+    # fs.delete(face.name)
+    # fs.delete(frame.name)
+
+    # return HttpResponse(base64.b64encode(open("media/output.jpg", "rb").read()), content_type="image/png")
 
