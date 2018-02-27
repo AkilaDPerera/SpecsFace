413f11dc285ee728a442e040fe75dde11b4e9641
akiladperera <akilamatrix@gmail.com>
2018-02-27 19:08:15 +0530

    No more file storing

--- API/models.py
+++ API/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
 
 # Create your models here.
+
+fs = FileSystemStorage()
+
+class Image(models.Model):
+    # url = models.URLField(unique=True) #The url to the evesite
+    image = models.ImageField(upload_to="", storage=fs)
--- API/views.py
+++ API/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 from django.http import FileResponse
 import base64
 from . import face_spec
+from .models import Image
 
 # Create your views here.
 def pageNotFound(request):
@@ -16,21 +17,31 @@ def dataUploadingForm(request):
 def processImages(request):
     face = request.FILES["face"]
     frame = request.FILES["frame"]
-    outputname = "output.jpg"
 
-    fs = FileSystemStorage()
-    face_url = fs.save(face.name, face)
-    frame_url = fs.save(frame.name, frame)
-    uploaded_face_url = settings.MEDIA_ROOT+"/"+face_url #fs.url(face_url)
-    uploaded_frame_url = settings.MEDIA_ROOT+"/"+frame_url  #fs.url(frame_url)
+    Iface, created = Image.objects.get_or_create(image=face)
+    Iframe, created = Image.objects.get_or_create(image=frame)
     
-    face_spec.process_image(uploaded_face_url, uploaded_frame_url, settings.MEDIA_ROOT+"/"+outputname)
-    
-    # Clear storage
-    fs.delete(face.name)
-    fs.delete(frame.name)
-    
-    img = open(settings.MEDIA_ROOT+"/"+outputname, "rb").read()
 
-    return HttpResponse(base64.b64encode(img), content_type="image/png")
+    # fs = FileSystemStorage()
+    # face_url = fs.save(face.name, face)
+    # frame_url = fs.save(frame.name, frame)
+    # uploaded_face_url = settings.MEDIA_ROOT+"/"+face_url #fs.url(face_url)
+    # uploaded_frame_url = settings.MEDIA_ROOT+"/"+frame_url  #fs.url(frame_url)
+    
+    # face_spec.process_image(face, frame)
+    
+    # # Clear storage
+    # fs.delete(face.name)
+    # fs.delete(frame.name)
+    
+    # img = open(settings.MEDIA_ROOT+"/"+outputname, "rb").read()
+    absFace = settings.MEDIA_ROOT + "/" + str(Iface.image)
+    absFrame = settings.MEDIA_ROOT + "/" + str(Iframe.image)
+
+    output = face_spec.process_image(absFace, absFrame)
+    # IOutput, created = Image.objects.get_or_create(image=output)
+
+    # absOutput = settings.MEDIA_ROOT + "/" + str(IOutput.image)
+
+    return HttpResponse(base64.b64encode(output), content_type="image/png")
 
